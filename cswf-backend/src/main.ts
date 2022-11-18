6c6e529753b7bcd6a3dc10e7ea734611ef6c179d
jwruijzenaars <56542348+jwruijzenaars@users.noreply.github.com>
2022-11-18 16:56:18 +0100

    changed cors options

--- cswf-backend/src/main.ts
+++ cswf-backend/src/main.ts
@@ -3,7 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors(
+    {
+      origin: ['*'],
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    }
+  );
 
   await app.listen(process.env.PORT || 3003);
   console.log(`app running at port: ${process.env.PORT}`);
